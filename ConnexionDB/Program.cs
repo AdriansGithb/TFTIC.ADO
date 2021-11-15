e217be796fa6a834d5b4b597ca98c28b4cc708d4
a.vanhoeke <a.vanhoeke@FORMAVDI1613>
2021-11-15 15:04:15 +0100

    Exe_SqlConnection

---
+++ ConnexionDB/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace ConnexionDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyConnectionString = @"Server=FORMAVDI1613\TFTIC;
+                                        Database=Ado;
+                                        Trusted_Connection=True;";
+
+            SqlConnection myConnection = new SqlConnection();
+            myConnection.ConnectionString = MyConnectionString;
+            Console.WriteLine(myConnection.State);
+            myConnection.Open();
+            Console.WriteLine(myConnection.State);
+            myConnection.Close();
+            Console.WriteLine(myConnection.State);
+
+            Console.ReadLine();   
+        }
+    }
+}
